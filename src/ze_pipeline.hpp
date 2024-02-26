a9d56b1d3e769cdbd5efece012dfac23087a384f
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-02-26 16:33:01 +0100

    part 5 2

--- src/ze_pipeline.hpp
+++ src/ze_pipeline.hpp
@@ -32,6 +32,7 @@ namespace ze {
         ZePipeline(const ZePipeline&) = delete;
         void operator=(const ZePipeline&) = delete;
 
+        void bind(VkCommandBuffer commandBuffer);
         static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
     private:
