1d95444e56f3dc1a5babe4cb20dd20f6007c2c09
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-02-29 09:46:40 +0100

    part 20

--- src/ze_app.cpp
+++ src/ze_app.cpp
@@ -16,8 +16,8 @@
 namespace ze {
 
     struct GlobalUbo {
-        glm::mat4 projectionView{1.0f};
-        glm::vec3 lightDirection = glm::normalize(glm::vec3{1.0f, -3.0f, -1.0f });
+        alignas(16) glm::mat4 projectionView{1.0f};
+        alignas(16) glm::vec3 lightDirection = glm::normalize(glm::vec3{1.0f, -3.0f, -1.0f });
     };
 
     ZeApp::ZeApp() {
